1ed307071f0a053a1b5da583a0a910a1fd99ed24
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-09-08 20:55:23 +0530

    if else,if else ladders

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+console.log("Hello this is conditionals!")
+let age = 14;
+let grace = 3;
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age / grace)
+console.log(age * grace)
+console.log(age ** grace)
+console.log(age % grace)
+
+if( age > 18){
+    console.log("You can drive!")
+}
+else
+{
+    console.log("you can not drive!")
+}
---
+++ conditionals1.js
@@ -0,0 +1,14 @@
+// if else ladder
+let age = 1;
+if (age == 18){
+    console.log("You can drive!")
+}
+else if(age == 0){
+    console.log("are you kidding?")
+}
+else if(age == 1){
+    console.log("are you again kidding?")
+}
+else{
+    console.log("you cant drive")
+}
