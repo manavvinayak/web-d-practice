f3cea29d7be0ccbe7bc70135b77acc3ef865167c
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-06 23:20:40 +0530

    Add files via upload

---
+++ Variables/switch.js
@@ -0,0 +1,15 @@
+const weather = prompt("What's the weather like? ")
+
+switch(weather) {
+    case "sunny":
+        alert("Cover up! ")
+         
+        case "rainy":
+            alert("Umbrella time! ")
+
+            case "dusty":
+                alert("Stay inside at home ")
+
+                default:
+                    alert("Not supported type! ")
+}
