e8bf907e606e23c95ad7bc3e9842f9a1ab93200d
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-10 16:39:38 +0530

    Add files via upload

---
+++ Variables/Modulo.js
@@ -0,0 +1,8 @@
+// This program uses the modulo operator %, which calculates the remainder after division of one number by another. It’s often used to assess number parity.
+
+for (let i; i<= 10; i++) {
+    if (i%2 === 0){
+        console.log(`${i} is even!`)
+    }
+    
+}
