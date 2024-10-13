b96ba67e29af4b404886b5dcf641ad385f920c57
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-13 15:53:32 +0530

    Delete Variables/Return.js

--- Variables/Return.js
+++
@@ -1,9 +0,0 @@
-// Creating a function
-function SayHello() {
-    // The keyword return indicates that the function will return a value, which is specified immediately after the keyword. This return value can be retrieved by the caller.
-  return "Hello ,welcome to this page";
-}
-// Calling the function
-
-const message = SayHello(); // Store the function return value in a variable
-console.log(message); // Show the return value
