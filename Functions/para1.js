333d37b0ea90db4346ee9b7c9f8084d3daed3d47
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-14 22:24:03 +0530

    Functions

---
+++ Functions/para1.js
@@ -0,0 +1,7 @@
+function intro(name, petname) {
+    console.log(`My name is ${name} & my petname is ${petname}! `)
+}
+// we have given two seperate for 2 parameters i.e. both are strings!
+intro('Vin', 'Bob');
+intro('Piy', 'Jack');
+intro('Ajay', 'kd');
