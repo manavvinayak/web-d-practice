c406beebee185c4e2dd2532a9efd2f6487c07ac7
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-18 23:33:38 +0530

    Minimum of Two Numbers

---
+++ Functions/Minimum.js
@@ -0,0 +1,17 @@
+
+function min(a,b){
+    if ( a > b)
+      console.log(a)
+    
+    else if(b > a)
+      console.log(b)
+    else
+       console.log(`a and b are same`);
+    
+  
+    // Write - Your - Code
+  
+  }
+  min(4.5, 5)
+  min(19, 9)
+  min(1, 1)
