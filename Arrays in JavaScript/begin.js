929acd40fae087dc173e765e83d1235c821f0f79
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-27 11:54:49 +0530

    Arrays in JavaScript

---
+++ Arrays in JavaScript/begin.js
@@ -0,0 +1,8 @@
+const movies = ["Swades", "Bose", "Shaheed"]
+console.log(movies)
+
+const array_check = ["string", {message: "Hi Mom"}, 79, true]
+console.log(array_check.length)
+
+const empty_array = []
+console.log(empty_array.length)
