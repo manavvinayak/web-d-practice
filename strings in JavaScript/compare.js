43473afa957cc43664ee3eabab6475043488d50e
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-29 22:07:20 +0530

    strings in JavaScript

---
+++ strings in JavaScript/compare.js
@@ -0,0 +1,4 @@
+const word = "Jack";
+console.log(word === "Oggy");
+console.log(word === "Jack");
+// String comparison is case sensitive. Do indeed pay attention to your lower and uppercase letters!
