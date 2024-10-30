ef84f58b689464a258b690b051f85d7e16337020
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-10-30 12:33:47 +0530

    strings in JavaScript

---
+++ strings in JavaScript/startsWith.js
@@ -0,0 +1,8 @@
+const song = "Give me some sunshine";
+// The startsWith method checks if a string begins with a specified substring. It returns true if the string starts with the substring, and false otherwise.
+console.log(song.startsWith("Give"));
+// The endsWith method checks if a string ends with a specified substring. It returns true if the string ends with the substring, and false otherwise.
+console.log(song.endsWith("sunshine"));
+console.log(song.startsWith("me"));
+console.log(song.endsWith("some"));
+console.log(song.startsWith("give"));
