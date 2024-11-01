1864ac67f3b3eb9e1b15a479263da4b0de6f4bfc
Vinayak <157000775+manavvinayak@users.noreply.github.com>
2024-11-01 15:13:38 +0530

    Add files via upload

---
+++ OOP in Js/OOP_Basics.js
@@ -0,0 +1,11 @@
+//  Classes
+// A class is like a blueprint for creating objects. It defines a set of properties and methods that the objects created from the class will have.
+
+// Mnemonic: Think of a class as a "Cookie Cutter" that shapes cookies (objects).
+
+// Objects
+// An object is an instance of a class. It is created using the blueprint provided by the class and has its own set of properties and methods.
+
+// Mnemonic: Think of an object as a "Cookie" made from the cookie cutter (class).
+
+// Most object-oriented languages use classes as abstractions for the ideas or concepts manipulated by a program. A class is used to create objects representing a concept. It offers a convenient syntax to give both data and behavior to these objects. JavaScript is no exception and supports programming with classes (but with a twist — more on that later).
