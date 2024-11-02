30cd97d36575929a6de7671ce3dfc9ce0237b40b
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2024-11-03 00:15:27 +0530

    Initial Commit

---
+++ strings in JavaScript/reverse.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example usage:
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
+console.log(reversedString); 
---
+++ strings in JavaScript/word.js
@@ -0,0 +1,18 @@
+word = "UniVersaL"
+function length(word){
+    let length = word.length;
+      console.log(length);
+}
+
+function lowerCase(word){
+   let lower = word.toLowerCase();
+      console.log(lower);
+}
+
+function upperCase(word){
+   let upper = word.toUpperCase();
+      console.log(upper);
+}
+length(word);
+lowerCase(word);
+upperCase(word);
